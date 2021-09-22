4dd2dbaa5f18882f6d5b9ea9fd2c912dd73b6a0a
jananisuji <jananisuji96@gmail.com>
2021-09-22 19:57:05 +0530

    changed

--- solution/Sep21/include/product.inc.php
+++ solution/Sep21/include/product.inc.php
@@ -11,17 +11,17 @@ if(isset($_POST['submit'])){
         //Add record details...
         add_record($con, $pname, $description, $price);
         
-        header('location:../profile.php?success=success');
+        header('location:../index.php?success=success');
     } else {
         //Update record details...
         update_record($con, $id, $pname, $description, $price);
-        header('location:../profile.php?success=success');
+        header('location:../index.php?success=success');
     }
 } elseif(isset($_GET['action']) && $_GET['action']=='del'){
     //Delete record details
     $id=$_GET['id'];
     delete_record($con,$id);
-    header('location:../profile.php?success=success');
+    header('location:../index.php?success=success');
 
 }
 ?>
