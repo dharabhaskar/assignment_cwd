14ecc78277e7ab69c59650eb03f052cab7ce612c
Bhaskar Dhara <dharabhaskar@gmail.com>
2021-09-24 21:10:53 +0530

    Ajax using fetch function javacript

--- solution/Sep23/add_product.php
+++ solution/Sep23/add_product.php
@@ -1,13 +1,21 @@
 <?php
 require_once('include/functions.inc.php');
 
-$pname = $_POST["pname"];
-$description = $_POST["description"];
-$price = $_POST["price"];
+
+
+// $pname = $_POST["pname"];
+// $description = $_POST["description"];
+// $price = $_POST["price"];
+
+
+$json = file_get_contents('php://input');
+$data=json_decode($json);
+//var_dump($json);
 
 $con=connect_db();
 
-$add_result = add_record($con, $pname, $description, $price);
+//$add_result = add_record($con, $pname, $description, $price);
+$add_result = add_record($con, $data->pname, $data->description, $data->price);
 $result = array();
 if ($add_result !== false) {
     $result['response_msg'] = 'Record added successfully.';
@@ -15,3 +23,5 @@ if ($add_result !== false) {
     $result['response_msg'] = 'Failed to add record.';
 }
 echo json_encode($result);
+
+
